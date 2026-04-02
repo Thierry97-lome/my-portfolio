06c7bcd79fa62dd3736648735dc95b8a34b542e2
Thierry97-lome <thierry.ogoubi@yahoo.com>
2026-04-02 17:59:48 -0400

    fisrt commit

---
+++ Funtime.py
@@ -0,0 +1,9 @@
+print("Hello Thierry, how is the robot project going?")
+print("I hope you are making good progress and that everything is working well.")
+print("If you need any help or have any questions, feel free to ask!")
+
+print("*")
+print("**")
+print("***")
+print("****")
+print("*****")
